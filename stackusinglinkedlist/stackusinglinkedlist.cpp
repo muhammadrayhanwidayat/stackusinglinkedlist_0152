7022e68bfda14a848f10d67c9bcfb00aa8c35fdb
muhammadrayhanwidayat <murayat99@gmail.com>
2024-05-11 10:43:02 +0700

    membuat fungsi peek

--- stackusinglinkedlist/stackusinglinkedlist.cpp
+++ stackusinglinkedlist/stackusinglinkedlist.cpp
@@ -45,8 +45,14 @@ public:
 		cout << "Popped value: " << top->data << endl;
 		top = top->next;//update the top pointer to the next node
 	}
-
-
+	//peek/top operation:retrieve the value of the topmost element without removing it
+	void peek()
+	{
+		if (top == NULL)
+		{
+			cout << "List is empty." << endl;
+		}
+	}
 };
 
 int main()
