788d8445581a89ba6e2f2b33a6fe0765774ab3e2
muhammadrayhanwidayat <murayat99@gmail.com>
2024-05-11 10:34:43 +0700

    membuat fungsi push

--- stackusinglinkedlist/stackusinglinkedlist.cpp
+++ stackusinglinkedlist/stackusinglinkedlist.cpp
@@ -18,14 +18,24 @@ public:
 class stack
 {
 private:
-	Node* top;
+	Node* top;//pointer to the top node of the stack
 
 public:
 	stack()
 	{
-		top = NULL;
+		top = NULL;//initialized the stack with a null top pointer
 	}
+	//push operation :insert an element onto top of the stack
+	int push(int value)
+	{
 
+		Node* newNode = new Node();//1.allocate memory for the new node
+		newNode->data = value;//2. assign value
+		newNode->next = top;//3.set the next pointer of the new node to the current
+		top = newNode;//4.update the top pointer to the new node
+		cout << "Push value: " << value << endl;
+		return value;
+	}
 }
 
 
